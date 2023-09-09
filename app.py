4dfcc567f5a246e5f42db65021c0aa37dadbe935
Rakib-data-scientist <rakib.ai.ds@gmail.com>
2023-09-09 14:34:56 +0530

    changes

--- app.py
+++ app.py
@@ -1,12 +1,27 @@
 import streamlit as st
-
 from utils import get_answer_csv
 
-st.header("Chat with any CSV")
-uploaded_file = st.file_uploader("Upload a csv file", type=["csv"])
+def display_header():
+    """Display the header of the Streamlit application."""
+    st.header("Interact with your pandas dataframe")
 
-if uploaded_file is not None:
-    query = st.text_area("Ask any question related to the document")
-    button = st.button("Submit")
-    if button:
-        st.write(get_answer_csv(uploaded_file, query))
+def upload_file():
+    """Allow user to upload a CSV file and return the uploaded file."""
+    return st.file_uploader("Upload a pandas dataframe", type=["csv"])
+
+def get_user_query():
+    """Prompt the user to enter a query and return the input."""
+    return st.text_area("Ask any question related to the dataframe")
+
+def main():
+    """Main function to orchestrate the Streamlit application flow."""
+    display_header()
+    uploaded_file = upload_file()
+    
+    if uploaded_file:
+        query = get_user_query()
+        if st.button("Submit"):
+            st.write(get_answer_csv(uploaded_file, query))
+
+if __name__ == "__main__":
+    main()
--- utils.py
+++ utils.py
@@ -1,22 +1,59 @@
-import os
 from typing import TextIO
-
+import os
 import openai
 import pandas as pd
 import streamlit as st
-from langchain.agents import create_csv_agent, create_pandas_dataframe_agent
+from langchain.agents import create_pandas_dataframe_agent
 from langchain.llms import OpenAI
 
+# Setting up openai API key from streamlit secrets
 openai.api_key = st.secrets["OPENAI_API_KEY"]
 
+def read_csv_file(file: TextIO) -> pd.DataFrame:
+    """
+    Reads data from a CSV file into a pandas DataFrame.
+
+    Args:
+        file (TextIO): The CSV file to read data from.
+
+    Returns:
+        pd.DataFrame: A pandas DataFrame containing the data from the CSV file.
+    """
+    return pd.read_csv(file)
+
+def create_agent(df: pd.DataFrame) -> "Agent":
+    """
+    Creates a Langchain agent with the specified dataframe.
+
+    Args:
+        df (pd.DataFrame): The dataframe to base the agent on.
+
+    Returns:
+        Agent: A Langchain agent.
+    """
+    return create_pandas_dataframe_agent(OpenAI(temperature=0), df, verbose=False)
 
 def get_answer_csv(file: TextIO, query: str) -> str:
-    df = pd.read_csv("Iris.csv")
+    """
+    Retrieves an answer to a query based on data in a CSV file.
 
-    # Create an agent using OpenAI and the Pandas dataframe
-    agent = create_pandas_dataframe_agent(OpenAI(temperature=0), df, verbose=False)
+    Args:
+        file (TextIO): The CSV file containing the data.
+        query (str): The query to find an answer to.
 
-    # Run the agent on the given query and return the answer
-    query = "whats is this dataframe about"
-    answer = agent.run(query)
-    return answer
+    Returns:
+        str: The answer to the query based on the data in the CSV file.
+    """
+    df = read_csv_file(file)
+    agent = create_agent(df)
+    return agent.run(query)
+
+# Streamlit UI
+if __name__ == "__main__":
+    st.title("CSV Query Agent")
+    uploaded_file = st.file_uploader("Upload a CSV file", type=["csv"])
+
+    if uploaded_file:
+        query = st.text_input("Enter your query:")
+        if query:
+            st.write(get_answer_csv(uploaded_file, query))
